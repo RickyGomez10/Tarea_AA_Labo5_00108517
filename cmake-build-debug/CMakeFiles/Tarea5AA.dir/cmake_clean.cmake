9a673be68780aeabe2dde360e3ccb2d6683a5d2f
RickyGomez10 <00108517@uca.edu.sv>
2019-09-24 20:54:00 -0600

    Tarea AA labo5 00108517 - POSTORDER

---
+++ cmake-build-debug/CMakeFiles/Tarea5AA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea5AA.dir/main.cpp.obj"
+  "Tarea5AA.pdb"
+  "Tarea5AA.exe"
+  "Tarea5AA.exe.manifest"
+  "libTarea5AA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea5AA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
